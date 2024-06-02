14bfbd0d4f8fe1be7d835e1bd5355a23d06585c8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 15:44:31 +0530

    first commit

---
+++ cmjd106-mvc/src/edu/ijse/mvc/Main.java
@@ -0,0 +1,18 @@
+
+package edu.ijse.mvc;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
