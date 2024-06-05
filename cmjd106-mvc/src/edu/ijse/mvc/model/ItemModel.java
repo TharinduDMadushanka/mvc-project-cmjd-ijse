fd24822a322e955568b2e0c00b04369c98f063bc
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 11:33:43 +0530

    first commit

--- cmjd106-mvc/src/edu/ijse/mvc/model/ItemModel.java
+++ cmjd106-mvc/src/edu/ijse/mvc/model/ItemModel.java
@@ -4,10 +4,33 @@
  */
 package edu.ijse.mvc.model;
 
+import edu.ijse.mvc.db.DBConnection;
+import edu.ijse.mvc.dto.ItemDto;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 /**
  *
  * @author ASUS
  */
 public class ItemModel {
     
+    private final Connection connection;
+    
+    public ItemModel() throws ClassNotFoundException,SQLException {
+        this.connection =DBConnection.getInstance().getConnetion();
+    }
+    
+    public String saveItem(ItemDto itemdto) throws Exception{
+        String sql ="INSERT INTO VALUES(?,?,?,?,?)";
+        
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1,itemdto.getCode());
+        statement.setString(2,itemdto.getDescription());
+        statement.setString(3,itemdto.getPackSize());
+        statement.setDouble(4,itemdto.getUnitPrize());
+        statement.setInt(5,itemdto.getQoh());
+        
+        return statement.executeUpdate() >0 ? "Success" : "Fail";
+    }
 }
