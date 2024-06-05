b27bc02a1ddf8873ad333c661e31c5b38dd99f47
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 10:25:51 +0530

    first commit

--- cmjd106-mvc/src/edu/ijse/mvc/Main.java
+++ cmjd106-mvc/src/edu/ijse/mvc/Main.java
@@ -12,7 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello");
+        
     }
     
 }
