994f7ff6b6b4684c1cc80f8702e0c4de79f2352e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 09:54:55 +0530

    first commit

--- cmjd106-mvc/src/edu/ijse/mvc/Main.java
+++ cmjd106-mvc/src/edu/ijse/mvc/Main.java
@@ -12,7 +12,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         new ItemView().setVisible(true);
     }
