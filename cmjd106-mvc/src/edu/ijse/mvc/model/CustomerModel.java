f22ee2228802693dc6e8fa36095546d560a924f6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 10:44:59 +0530

    first commit

--- cmjd106-mvc/src/edu/ijse/mvc/model/CustomerModel.java
+++ cmjd106-mvc/src/edu/ijse/mvc/model/CustomerModel.java
@@ -5,8 +5,10 @@
 package edu.ijse.mvc.model;
 
 import edu.ijse.mvc.db.DBConnection;
+import edu.ijse.mvc.dto.CustomerDto;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 
 /**
@@ -14,9 +16,26 @@ import java.sql.SQLException;
  * @author ASUS
  */
 public class CustomerModel {
-    private Connection connection1;
+    private Connection connection;
     
    public CustomerModel() throws ClassNotFoundException, SQLException {
-        this.connection1 =  DBConnection.getInstance().getConnection();
+        this.connection =  DBConnection.getInstance().getConnection();
     }
+   
+   public String saveCustomer(CustomerDto customerDto) throws SQLException{
+       String sql ="INSERT INTO customer VALUES(?,?,?,?,?,?,?,?,?)";
+       
+       PreparedStatement statement =connection.prepareStatement(sql);
+       
+       statement.setString(1, customerDto.getId());
+       statement.setString(2, customerDto.getTitle());
+       statement.setString(3, customerDto.getName());
+       statement.setString(4, customerDto.getDob());
+       statement.setDouble(5, customerDto.getSalary());
+       statement.setString(6, customerDto.getAddress());
+       statement.setString(7, customerDto.getCity());
+       statement.setString(8, customerDto.getProvince());
+       statement.setString(9, customerDto.getPostal());
+      
+   }
 }
