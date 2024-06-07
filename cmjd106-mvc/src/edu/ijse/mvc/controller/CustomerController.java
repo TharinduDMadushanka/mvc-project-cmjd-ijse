ebaf04fad77defeafe2748aafa8b2e8a9ecfe345
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 10:57:52 +0530

    first commit

--- cmjd106-mvc/src/edu/ijse/mvc/controller/CustomerController.java
+++ cmjd106-mvc/src/edu/ijse/mvc/controller/CustomerController.java
@@ -4,10 +4,23 @@
  */
 package edu.ijse.mvc.controller;
 
+import edu.ijse.mvc.dto.CustomerDto;
+import edu.ijse.mvc.model.CustomerModel;
+import java.sql.SQLException;
+
 /**
  *
  * @author ASUS
  */
 public class CustomerController {
+    private CustomerModel customerModel;
     
+    public CustomerController() throws Exception{
+        this.customerModel=new CustomerModel();
+    }
+    
+    public String saveCustomer(CustomerDto customerDto) throws Exception{
+        String resp =customerModel.saveCustomer(customerDto);
+        return resp;
+    }
 }
